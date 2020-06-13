e1049cf45910f12afdebca51a3be37c6af3b1c70
Jabbar <multimediary@gmail.com>
2020-06-14 00:32:03 +0700

    Update 3.php

--- 3.php
+++ 3.php
@@ -1,6 +1,24 @@
+<table border="1">
 <?php
 function cetak_gambar($bilangan){
-	
+	for($i = 0; $i <$bilangan; $i++) 
+    { 
+		if($i % 2 == 0){ 
+			for ($j = 0; $j < $bilangan; $j++) 
+			{ 
+				if($j==2){ echo "="; }else{ echo "*"; }
+			
+			}
+        }
+		if($i % 2 != 0){ 
+			for ($j = 0; $j < $bilangan; $j++) 
+			{ 
+				if($j==1 || $j==3){ echo "="; }else{ echo "*"; }
+			
+			}
+        }
+        echo "<br>"; 
+    } 
 }
 
 echo cetak_gambar(5)
